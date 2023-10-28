19c286efeed24ff13de2f3bbd14f2fb634e6021b
Mert Aytemiz <mertaytemizwork@gmail.com>
2023-10-28 14:40:26 +0300

    second commit

--- docker-nextjs/next.config.js
+++ docker-nextjs/next.config.js
@@ -3,4 +3,17 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+// module.exports = {
+//   ...nextConfig, // İlk yapılandırmayı dahil edin
+//   // Diğer ayarlar buraya eklenebilir
+//   webpackDevMiddleware: (config) => {
+//     // API rotalarının değişikliklerini izlemesini sağlar
+//     config.watchOptions = {
+//       poll: 1000, // Değişiklikleri her 1 saniyede bir izler
+//       aggregateTimeout: 300, // 300 ms içindeki tüm değişiklikleri birleştirir
+//     };
+//     return config;
+//   },
+// };
--- docker-nextjs/src/pages/api/hello.js
+++ docker-nextjs/src/pages/api/hello.js
@@ -1,6 +1,26 @@
-export default function handler(req, res) {
-  const apiEndpoint = process.env.API_ENDPOINT;
-  res.status(200).json({ name: 'John Doe' });
-}
+export async function fetchPersonData() {
+  const url = process.env.NEXT_PUBLIC_API_URL;
+  const token = process.env.NEXT_PUBLIC_API_TOKEN;
 
-// const apiEndpoint = process.env.API_ENDPOINT || 'http://localhost:3000/api/hello';
+  if (!url || !token) {
+    console.error("API_URL and API_TOKEN environment variables are required.");
+    throw new Error("Missing environment variables");
+  }
+
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  };
+
+  try {
+    const response = await fetch(url, options);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching person data:", error);
+    throw new Error("Error fetching person data");
+  }
+}
